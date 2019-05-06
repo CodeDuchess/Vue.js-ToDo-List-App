6cd316a1b94d63762250ac9f0f5bedd33493915d
Dana L. Brown <dana.l.brown@gmail.com>
2019-05-06 12:02:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
 "use strict";
 
 	Vue.directive("auto-focus", {
-	  bind: function () {
-	   Vue.nextTick(function () {
-		this.el.focus();
-		}.bind(this));
+		bind: function () {
+			Vue.nextTick(function () {
+				this.el.focus();
+			}.bind(this));
 		}
-	    });
+	});
 
 	new Vue({
 		
 		el: "#todo",
+		
 		data: {
-		  newTask: "",
-		  tasks: [
-		   {
-		    text: "This is an example task. Delete or add your own",
-		    checked: false
-		     }
-		    ],
+			newTask: "",
+			tasks: [
+				{
+					text: "This is an example task. Delete or add your own",
+					checked: false
+				}
+			],
 
-		    editingTask: {
+			editingTask: {
 
-		  }
+			}
 		},
 		
 		computed: {
 			areAllSelected: function () {
 				return this.tasks.every(function(task) {
-				return task.checked;
+					return task.checked;
 				}) &&  this.tasks.length > 0;
 			},
 		},
 
 		methods: {
+
 			addTask: function () {
-			  var task = this.newTask.trim();
-			  if (task) {
-			  this.tasks.push({text: task, checked: false});
-			  this.newTask = "";
-			  }
-			 },
+				var task = this.newTask.trim();
+				if (task) {
+					this.tasks.push({text: task, checked: false});
+					this.newTask = "";
+				}
+			},
 
 			removeTask: function (task) {
-                        var index = this.tasks.indexOf(task);
-			this.tasks.splice(index, 1);
+        var index = this.tasks.indexOf(task);
+				this.tasks.splice(index, 1);
 			},
 
 			editTask: function (task) {
@@ -55,19 +57,21 @@
 				this.editingTask = {};
 				if (task.text.trim() === ""){
 					this.removeTask(task);
-		           }
-			 },
+				}
+				
+			},
 
 			clearList: function () {
 				this.tasks = [
-			  ];
+
+				];
 			},
 
 			selectAll: function (task) {
-				var targetValue = this.areAllSelected ? false : true;true;
+				var targetValue = this.areAllSelected ? false : true;
 				for (var i = 0; i < this.tasks.length; i++) {
 					this.tasks[i].checked = targetValue;
-			  }
+				}
 			},
 
 			check: function (task) {
@@ -77,5 +81,6 @@
 			isChecked: function (task) {
 				return task.checked;
 			}
-	             }
-	        });
+
+		}
+	});
